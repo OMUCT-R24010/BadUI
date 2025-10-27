5cbc9fd096e8a93025b56b311a5cbd452467f179
OMUCT-R24010 <rg24010r@st.omu.ac.jp>
2025-10-27 11:04:02 +0900

    Setup react vite

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 以下の 'base' の行を追加
+  base: '/BadUI/',
+})
